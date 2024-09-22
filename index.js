2893147b8faf68f9e7b78632c544b956d97a32bc
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-23 00:08:54 +0200

    Added a file path

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const path = require('path');
 // Directory and file paths
 const directory = path.join(__dirname, 'shopping-list');
 const filePath = path.join(directory, 'shopping-list.json');
+
+// Ensure directory exists
+if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+  
+  // Ensure file exists
+  if (!fs.existsSync(filePath)) {
+    // Create shopping list file
+    fs.writeFileSync(filePath, JSON.stringify([])); 
+  }
