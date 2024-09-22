22fbda951b63588c22d034e3455c69d19cee2f7f
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-22 23:52:14 +0200

    Added file directory and path to the file

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const fs = require('fs');
 // Path
 const path = require('path');
 
+// Directory and file paths
+const directory = path.join(__dirname, 'shopping-list');
+const filePath = path.join(directory, 'shopping-list.json');
